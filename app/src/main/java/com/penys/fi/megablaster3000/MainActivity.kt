65c95447951c9cf61679b02c2985ce270b7566b6
pessi22 <juhopessi1@gmail.com>
2018-09-13 10:06:00 +0300

    plane and 60 dps

--- app/src/main/java/com/penys/fi/megablaster3000/MainActivity.kt
+++ app/src/main/java/com/penys/fi/megablaster3000/MainActivity.kt
@@ -1,92 +1,85 @@
 package com.penys.fi.megablaster3000
 
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.ImageView
-import com.google.ar.core.*
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.core.Pose
+import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.CompletableFuture
+
+
 class MainActivity : AppCompatActivity() {
 
+    lateinit var testRenderable: ModelRenderable
     lateinit var fragment: ArFragment
-    lateinit var fitToScanImageView: ImageView
-    lateinit var duckyRenderable: ModelRenderable
-    lateinit var miniRenderable: ModelRenderable
+    lateinit var renderableFuture: CompletableFuture<ModelRenderable>
+    lateinit var modelUri: Uri
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fragment = supportFragmentManager.findFragmentById(R.id.arimage_fragment) as ArFragment
-        fitToScanImageView = findViewById(R.id.fit_to_scan_img)
 
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as
+                ArFragment
 
-        val ducky = ModelRenderable.builder()
-                .setSource(this, Uri.parse("ducky.sfb"))
-                .build()
+        modelUri = Uri.parse("ducky.sfb")
 
-        ducky.thenAccept { it -> duckyRenderable = it }
+        renderableFuture = ModelRenderable.builder()
+                .setSource(this, modelUri).build()
 
-        val mini = ModelRenderable.builder()
-                .setSource(this, Uri.parse("mini_obj.sfb"))
-                .build()
+        renderableFuture.thenAccept { it -> testRenderable = it }
 
-        mini.thenAccept { it -> miniRenderable = it }
+        puttis.isClickable = false
 
-        fragment.arSceneView.scene.addOnUpdateListener { frameTime ->
-            onUpdate(frameTime)
+        puttis.setOnClickListener {
+            addObject()
+        }
 
+        fragment.arSceneView.scene.addOnUpdateListener {
+            if(fragment.arSceneView.arFrame.camera.trackingState == TrackingState.TRACKING) {
+                addObject()
+            } else {
+
+            }
         }
     }
 
-    private fun onUpdate(frameTime: FrameTime) {
-        fragment.onUpdate(frameTime)
-        val arFrame = fragment.arSceneView.arFrame
-        if (arFrame == null || arFrame.camera.trackingState != TrackingState.TRACKING) {
-            return
-        }
-        val updatedAugmentedImages = arFrame.getUpdatedTrackables(AugmentedImage::class.java)
-        updatedAugmentedImages.forEach {
-            when (it.trackingState) {
-                TrackingState.PAUSED -> {
+    val pose = Pose.makeTranslation(0.0f, 0.0f, -1.3f)
 
-                }
-
-                TrackingState.STOPPED
-                -> {
-
-                }
-            }
-        }
-
-        updatedAugmentedImages.forEach {
-            when (it.trackingState) {
-
-                TrackingState.TRACKING
-                -> {
-                    var anchors = it.anchors
-                    if (anchors.isEmpty()) {
-                        fitToScanImageView.visibility = View.GONE
-                        val pose = Pose.makeTranslation(0.0f, 0.0f, -1.3f)
-                        val anchor = it.createAnchor(pose)
-                        val anchorNode = AnchorNode(anchor)
-                        anchorNode.setParent(fragment.arSceneView.scene)
-                        val imgNode = TransformableNode(fragment.transformationSystem)
-                        imgNode.setParent(anchorNode)
-                        if (it.name == "tits") {
-                            imgNode.renderable = duckyRenderable
-                        } else {
-                            imgNode.renderable = miniRenderable
-                        }
-                    }
-
+    private fun addObject() {
+        val frame =  fragment.arSceneView.arFrame
+        val pt = getScreenCenter()
+        val hits: List<HitResult>
+        if(frame != null && testRenderable != null) {
+            hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+            for(hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane) {
+                    val anchor = hit!!.createAnchor()
+                    val anchorNode = AnchorNode(anchor)
+                    anchorNode.setParent(fragment.arSceneView.scene)
+                    val mNode = TransformableNode(fragment.transformationSystem)
+                    mNode.setParent(anchorNode)
+                    mNode.renderable = testRenderable
+                    mNode.select()
+                    break
                 }
             }
         }
+    }
+
+    private fun getScreenCenter(): android.graphics.Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return android.graphics.Point(vw.width/2, vw.height/ 2)
     }
 }
+
