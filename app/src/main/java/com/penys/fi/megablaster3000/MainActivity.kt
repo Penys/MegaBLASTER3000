34822406c535c14f687726ef0da6c7d7ff245046
pessi22 <juhopessi1@gmail.com>
2018-09-13 14:57:28 +0300

    säätings

--- app/src/main/java/com/penys/fi/megablaster3000/MainActivity.kt
+++ app/src/main/java/com/penys/fi/megablaster3000/MainActivity.kt
@@ -2,18 +2,18 @@ package com.penys.fi.megablaster3000
 
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.provider.Telephony
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
-import com.google.ar.core.Pose
 import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.CompletableFuture
 
 
@@ -23,7 +23,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var fragment: ArFragment
     lateinit var renderableFuture: CompletableFuture<ModelRenderable>
     lateinit var modelUri: Uri
-     var nro: Int = 0
+
+    var nro: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,74 +40,90 @@ class MainActivity : AppCompatActivity() {
 
         renderableFuture.thenAccept { it -> testRenderable = it }
 
-        puttis.setOnClickListener {
 
 
 
 
-            fragment.arSceneView.scene.addOnUpdateListener {
-                if (fragment.arSceneView.arFrame.camera.trackingState == TrackingState.TRACKING) {
-                    for (i in 1..4)
-                        addObject()
+        fragment.arSceneView.scene.addOnUpdateListener {
+            if (fragment.arSceneView.arFrame.camera.trackingState == TrackingState.TRACKING) {
 
-                }
-            
-                    puttis.isClickable = false
-                }
-
+                addObject()
+            }
         }
+
+
     }
 
-    var rndm = 0.1f + Math.random() * (0.5f - 0.1f)
+    private fun rndm(): Float {
+        var rndm = 0.1f + Math.random() * (0.5f + 0.1f)
+        return rndm.toFloat()
+
+    }
+
 
     private fun addObject() {
-        val frame =  fragment.arSceneView.arFrame
+        val frame = fragment.arSceneView.arFrame
         val pt = getScreenCenter()
         val hits: List<HitResult>
-        if(frame != null && testRenderable != null) {
-            hits = frame.hitTest(0.1f, 0.2f)
-            for(hit in hits) {
+        if (frame != null) {
+
+            hits = frame.hitTest(rndm(), rndm())
+
+            for (hit in hits) {
                 val trackable = hit.trackable
                 if (trackable is Plane) {
-
 
                     val anchor = hit!!.createAnchor()
                     val anchorNode = AnchorNode(anchor)
                     anchorNode.setParent(fragment.arSceneView.scene)
                     val mNode = TransformableNode(fragment.transformationSystem)
-                    mNode.setParent(anchorNode)
-                    if(nro <= 4) {
-                    mNode.renderable = testRenderable
+                    if (nro <= 2) {
 
+                        mNode.setParent(anchorNode)
+
+
+                        mNode.renderable = testRenderable
 
                         mNode.select()
-                    }else {
-                        break
-                    }
-
-
-                    mNode.setOnTapListener { _, _ ->
-                    anchorNode.removeChild(mNode)
-
-                        nro--
-
+                        nro++
                         Log.d("NUMERO", "$nro")
 
-                        addObject()
-                        return@setOnTapListener
+                    }
+
+                    mNode.setOnTapListener { _, _ ->
+                        anchorNode.removeChild(mNode)
+
+                        nro--
+                        Log.d("NUMERO", "$nro")
 
                     }
-                    nro++
-                    Log.d("NUMERO", "$nro")
-                    break
+                } else {
+
+                    return
                 }
+
+
+
+                //break
             }
         }
+
+    }
+
+    fun withDelay(delay: Long, block: () -> Unit) {
+        Handler().postDelayed(Runnable(block), delay)
     }
 
     private fun getScreenCenter(): android.graphics.Point {
         val vw = findViewById<View>(android.R.id.content)
-        return android.graphics.Point(vw.width/2, vw.height/ 2)
+        return android.graphics.Point(vw.width / 2, vw.height / 2)
+    }
+
+    private inner class SimpleRunnable : Runnable {
+        public override fun run() {
+            addObject()
+
+        }
     }
 }
 
