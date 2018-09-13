cd85292d1c546c021ccbf6cdd5faece1cdcbcd6e
anttinym <nyman.antti@gmail.com>
2018-09-13 15:57:23 +0300

    added sounds and blast counter for blasted ducks

--- app/src/main/java/com/penys/fi/megablaster3000/MainActivity.kt
+++ app/src/main/java/com/penys/fi/megablaster3000/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.penys.fi.megablaster3000
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
-import android.provider.Telephony
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.core.TrackingState
@@ -14,6 +15,7 @@ import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.CompletableFuture
 
 
@@ -23,39 +25,28 @@ class MainActivity : AppCompatActivity() {
     lateinit var fragment: ArFragment
     lateinit var renderableFuture: CompletableFuture<ModelRenderable>
     lateinit var modelUri: Uri
+    lateinit var duck_sound: MediaPlayer
 
+    var blast_count = 0
     var nro: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as
-                ArFragment
-
-        modelUri = Uri.parse("ducky.sfb")
-
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
+        blast_counter.text = "Ducks blasted: " + blast_count.toString()
+        modelUri = Uri.parse("RubberDuck.sfb")
+        duck_sound = MediaPlayer.create(this, R.raw.duck)
         renderableFuture = ModelRenderable.builder()
                 .setSource(this, modelUri).build()
-
         renderableFuture.thenAccept { it -> testRenderable = it }
 
-
-
-
-
         fragment.arSceneView.scene.addOnUpdateListener {
-<<<<<<< HEAD
-            if(fragment.arSceneView.arFrame.camera.trackingState == TrackingState.TRACKING) {
-=======
             if (fragment.arSceneView.arFrame.camera.trackingState == TrackingState.TRACKING) {
-
->>>>>>> 34822406c535c14f687726ef0da6c7d7ff245046
                 addObject()
             }
         }
-
-
     }
 
     private fun rndm(): Float {
@@ -64,60 +55,44 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
     private fun addObject() {
         val frame = fragment.arSceneView.arFrame
         val pt = getScreenCenter()
         val hits: List<HitResult>
-<<<<<<< HEAD
-        if(frame != null && testRenderable != null) {
-            hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
-            for(hit in hits) {
-=======
         if (frame != null) {
-
             hits = frame.hitTest(rndm(), rndm())
-
             for (hit in hits) {
->>>>>>> 34822406c535c14f687726ef0da6c7d7ff245046
                 val trackable = hit.trackable
                 if (trackable is Plane) {
-
                     val anchor = hit!!.createAnchor()
                     val anchorNode = AnchorNode(anchor)
                     anchorNode.setParent(fragment.arSceneView.scene)
                     val mNode = TransformableNode(fragment.transformationSystem)
                     if (nro <= 2) {
-
                         mNode.setParent(anchorNode)
-
-
                         mNode.renderable = testRenderable
-
                         mNode.select()
+                        duck_sound.start()
                         nro++
                         Log.d("NUMERO", "$nro")
 
                     }
-
                     mNode.setOnTapListener { _, _ ->
+                        duck_sound.start()
+                        blast_count++
+                        blast_counter.text = "Ducks blasted: " + blast_count.toString()
                         anchorNode.removeChild(mNode)
-
                         nro--
                         Log.d("NUMERO", "$nro")
-
                     }
                 } else {
 
                     return
                 }
 
-
-
                 //break
             }
         }
-
     }
 
     fun withDelay(delay: Long, block: () -> Unit) {
